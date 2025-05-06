4fbab2903e72b634a1004b808329bafb47cead9f
mike-sazonov <mihailsazon@gmail.com>
2025-05-06 18:47:17 +0300

    Change: consumer.py

---
+++ app/workers/consumer.py
@@ -0,0 +1,88 @@
+import asyncio
+import json
+
+from aio_pika import connect_robust, RobustConnection, RobustChannel, IncomingMessage
+
+from app.core.logger import logger
+from app.core.config import settings
+from app.utils.unitofwork import IUnitOfWork
+from app.db.models import TaskStatus
+
+
+class TaskWorker:
+    def __init__(self, rabbitmq_url: str, queue_name: str = "task_queue"):
+        self.rabbitmq_url = rabbitmq_url
+        self.queue_name = queue_name
+        self.connection: RobustConnection | None = None
+        self.channel: RobustChannel | None = None
+        self.queue = None
+
+
+    async def connect(self):
+        self.connection = await connect_robust(self.rabbitmq_url)
+        self.channel = await self.connection.channel()
+        self.queue = await self.channel.declare_queue(self.queue_name, durable=True, arguments={'x-max-priority': 10})
+
+
+    async def handle_message(self, message: IncomingMessage):
+        async with message.process():
+            payload = json.loads(message.body)
+
+            task_id = payload.get("task_id")
+            if not task_id:
+                logger.info("Received message without task_id")
+
+            logger.info(f"Processed task {task_id}")
+
+
+    async def process_task(self, task_id: str):
+            async with IUnitOfWork() as uow:
+                task = await uow.task.find_one(id=task_id)
+
+                if not task:
+                    logger.info(f"Task {task_id} not found")
+                    return
+
+                if task.status != TaskStatus.PENDING:
+                    logger.info(f"Wrong status for task {task_id}")
+                    return
+
+                task.status = TaskStatus.IN_PROGRESS
+                await uow.commit()
+
+            try:
+                await asyncio.sleep(2)
+
+
+                async with IUnitOfWork() as uow:
+                    task = await uow.task.find_one(id=task_id)
+                    if not task:
+                        return
+
+                    task.status = TaskStatus.COMPLETED
+                    await uow.commit()
+                logger.info(f"Task {task_id} completed")
+
+            except Exception as e:
+                logger.error(f"Task {task_id} failed: {str(e)}")
+                if task:
+                    task.status = TaskStatus.FAILED
+                    await uow.commit()
+
+
+    async def start(self):
+        await self.connect()
+        logger.info("Worker is up")
+
+        await self.queue.consume(self.handle_message)
+
+        while True:
+            await asyncio.sleep(1)
+
+async def run_worker():
+    worker = TaskWorker(rabbitmq_url=settings.RABBITMQ_URL)
+    await worker.start()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_worker())
